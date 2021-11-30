595d4af1c08261c05cd2eda0e0241893cd1ed5e5
eugeneoei <eugeneoei89@gmail.com>
2021-11-30 09:15:14 +0800

    populate user information for each tweet

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -7,13 +7,20 @@ class TweetSerializer(serializers.ModelSerializer):
 
     content = serializers.CharField(required=True)
     '''
-    Populate user details in each tweet
+    Or populating whole User object based on serializer
     Resources:
     - https://stackoverflow.com/questions/61617985/creating-a-serializer-to-work-with-model-relationships
+    TODO:
+    1. How to populate selected fields?
     '''
-    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
     user_details = UserSerializer(source='user', read_only=True)
 
     class Meta:
         model = Tweet
-        fields = ('id', 'user', 'user_details', 'content', 'created_at', 'updated_at')
+        fields = (
+            'id',
+            'content',
+            'created_at',
+            'updated_at',
+            'user_details'
+        )
