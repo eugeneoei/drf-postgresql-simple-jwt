1bdd1bfcf3151b280e219127b3405b123caeade8
eugeneoei <eugeneoei89@gmail.com>
2021-12-01 09:56:06 +0800

    return specific fields of user who posted tweet in each tweet

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -1,18 +1,43 @@
 from rest_framework import serializers
 from .models import Tweet
 from users.serializers import CustomUserSerializer as UserSerializer
+from users.models import CustomUser as User
+
+
+class TweetUserSerializer(serializers.ModelSerializer):
+
+    first_name = serializers.CharField(read_only=True)
+    last_name = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'first_name',
+            'last_name'
+        )
+
 
 class TweetSerializer(serializers.ModelSerializer):
 
     content = serializers.CharField(required=True)
+
     '''
-    Or populating whole User object based on serializer
+    Populate parent object
     Resources:
     - https://stackoverflow.com/questions/61617985/creating-a-serializer-to-work-with-model-relationships
-    TODO:
-    1. How to populate selected fields?
     '''
+    # this approach populates parent object based on parents's serializer.
+    # in this case, populates fields defined in UserSerializer
     user_details = UserSerializer(source='user', read_only=True)
+    # this approach populates specific fields in parent object using a different serializer
+    # in this case, populates fields defined in TweetUserSerializer
+    user = TweetUserSerializer()
+
+    '''
+    QUESTIONS:
+    - why "user_details" variable can be replaced with another variable name but not "user" variable?
+    '''
 
     class Meta:
         model = Tweet
@@ -22,4 +47,5 @@ class TweetSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
             'user_details',
+            'user'
         )
